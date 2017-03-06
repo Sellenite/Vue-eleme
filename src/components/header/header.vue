ee63b4996ead3858748b8085a6306f3bc77339d4
Sellenite <714627528@qq.com>
2017-03-06 14:39:33 +0800

    header complete

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -14,12 +14,23 @@
                 </div>
                 <!--防止一开始取不到supports，报undefined-->
                 <div v-if="seller.supports" class="support">
-                    <span class="icon"></span>
+                    <span class="icon" v-bind:class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
+            <div v-if="seller.supports" class="support-count">
+                <span class="count">{{seller.supports.length}}个</span>
+                <i class="icon-keyboard_arrow_right"></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+            <span class="bulletin-title"></span>
+            <span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img v-bind:src="seller.avatar" width="100%" height="auto">
+        </div>
     </div>
 </template>
 
@@ -34,6 +45,9 @@
             minFromat: function (value) {
                 return value + '分钟';
             }
+        },
+        created() {
+            this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
         }
     };
 </script>
@@ -42,17 +56,21 @@
     @import '../../common/stylus/mixin.styl'
 
     .header
+        position : relative
         color : #FFF
-        background : #000
+        background : rgba(7, 17, 27, 0.5)
         .content-wrapper
+            position : relative
             padding : 24px 12px 28px 24px
             font-size : 0
             .avatar
                 display : inline-block
                 font-size : 14px
+                img
+                    border-radius : 2px
             .content
                 display : inline-block
-                font-size : 14px
+                vertical-align : top
                 margin-left : 16px
                 .title
                     margin : 2px 0 8px 0
@@ -69,4 +87,82 @@
                         font-size : 16px
                         line-height : 18px
                         font-weight : bold
+                .description
+                    margin-bottom : 10px
+                    line-height : 12px
+                    font-size : 12px
+                .support
+                    .icon
+                        display : inline-block
+                        vertical-align : top
+                        width : 12px
+                        height : 12px
+                        margin-right : 4px
+                        background-size : 12px 12px
+                        background-repeat : no-repeat
+                        &.decrease
+                            bg-image('decrease_1')
+                        &.discount
+                            bg-image('discount_1')
+                        &.guarantee
+                            bg-image('guarantee_1')
+                        &.invoice
+                            bg-image('invoice_1')
+                        &.special
+                            bg-image('special_1')
+                    .text
+                        font-size : 10px
+                        line-height : 12px
+            .support-count
+                position : absolute
+                right : 12px
+                bottom : 26px
+                padding : 0 8px
+                height : 24px
+                line-height : 24px
+                border-radius : 14px
+                background : rgba(0,0,0,0.2)
+                font-size : 10px
+                text-align : center
+                .icon-keyboard_arrow_right
+                    vertical-align : middle
+                    line-height : 24px
+                    margin-left : 2px
+        .bulletin-wrapper
+            position : relative
+            height : 28px
+            line-height : 28px
+            padding : 0 22px 0 12px
+            white-space : nowrap
+            overflow : hidden
+            text-overflow : ellipsis
+            background : rgba(7,17,27.0.2)
+            .bulletin-title
+                display : inline-block
+                vertical-align : middle
+                width : 22px
+                height : 12px
+                bg-image('bulletin')
+                background-size : 22px 12px
+                background-repeat : no-repeat
+            .bulletin-text
+                margin : 0 4px 0 2px
+                font-size : 10px
+            .icon-keyboard_arrow_right
+                position : absolute
+                font-size : 10px
+                right : 12px
+                top : 10px
+        .background
+            position : absolute
+            top : 0
+            left : 0
+            width : 100%
+            height : 100%
+            z-index : -1
+            overflow : hidden
+            img
+                filter : blur(10px)
+                position : relative
+                top : -85%
 </style>
