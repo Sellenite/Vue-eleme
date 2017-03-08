30360af0b9955250492807871d1ecbec3f5f6d2e
Sellenite <714627528@qq.com>
2017-03-08 10:44:53 +0800

    install better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="goods">
-        <div class="menu-wrapper">
+        <div class="menu-wrapper" ref="menuWrapper">
             <ul>
                 <li v-for="(item, index) in goods" class="menu-item">
                     <span class="text border-1px">
@@ -9,7 +9,7 @@
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper">
+        <div class="foods-wrapper" ref="foodsWrapper">
             <ul>
                 <li v-for="(item, index) in goods" class="food-list">
                     <h1 class="title">{{item.name}}</h1>
@@ -39,7 +39,10 @@
 </template>
 
 <script type="text/ecmascript-6">
+    import BScroll from 'better-scroll';
+
     const ERR_OK = 0;
+
     export default {
         props: {
             seller: {
@@ -56,9 +59,18 @@
                 response = response.body;
                 if (response.errno === ERR_OK) {
                     this.goods = response.data;
+                    this.$nextTick(() => {
+                        this._initScroll();
+                    });
                 }
             });
             this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+        },
+        methods: {
+            _initScroll() {
+                this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+                this.foodScroll = new BScroll(this.$refs.foodsWrapper, {});
+            }
         }
     };
 </script>
@@ -66,31 +78,31 @@
 <style lang="stylus" rel="stylesheet/stylus">
     @import "../../common/stylus/mixin.styl"
     .goods
-        display : flex
-        position : absolute
-        width : 100%
-        top : 186px
-        bottom : 46px
-        overflow : hidden
+        display: flex
+        position: absolute
+        width: 100%
+        top: 186px
+        bottom: 46px
+        overflow: hidden
         .menu-wrapper
-            flex : 0 0 90px
+            flex: 0 0 90px
             /* 兼容性问题，安卓 */
-            width : 80px
-            background : #F3F5F7
+            width: 80px
+            background: #F3F5F7
             .menu-item
-                display : table
-                height : 54px
-                width : 56px
-                line-height : 14px
-                padding : 0 17px
+                display: table
+                height: 54px
+                width: 56px
+                line-height: 14px
+                padding: 0 17px
                 .icon
-                    display : inline-block
-                    vertical-align : top
-                    width : 12px
-                    height : 12px
-                    margin-right : 2px
-                    background-size : 12px 12px
-                    background-repeat : no-repeat
+                    display: inline-block
+                    vertical-align: top
+                    width: 12px
+                    height: 12px
+                    margin-right: 2px
+                    background-size: 12px 12px
+                    background-repeat: no-repeat
                     &.decrease
                         bg-image('decrease_3')
                     &.discount
@@ -102,60 +114,60 @@
                     &.special
                         bg-image('special_3')
                 .text
-                    display : table-cell
-                    width : 56px
-                    vertical-align : middle
-                    font-size : 12px
+                    display: table-cell
+                    width: 56px
+                    vertical-align: middle
+                    font-size: 12px
                     border-1px(rgba(7, 17, 27, 0.1))
         .foods-wrapper
-            flex : 1
+            flex: 1
             .title
-                padding-left : 14px
-                height :  26px
-                line-height : 26px
-                border-left : 2px solid #D9DDE1
-                font-size : 12px
-                color : rgb(147, 153, 159)
-                background : #F3F5F7
+                padding-left: 14px
+                height: 26px
+                line-height: 26px
+                border-left: 2px solid #D9DDE1
+                font-size: 12px
+                color: rgb(147, 153, 159)
+                background: #F3F5F7
             .food-item
-                display : flex
-                margin : 18px
-                padding-bottom : 18px
+                display: flex
+                margin: 18px
+                padding-bottom: 18px
                 border-1px(rgba(7, 17, 27, 0.1))
                 &:last-child
                     border-none()
-                    margin-bottom : 0
+                    margin-bottom: 0
                 .icon
-                    flex : 0 0 57px
-                    margin-right : 10px
+                    flex: 0 0 57px
+                    margin-right: 10px
                 .content
-                    flex : 1
+                    flex: 1
                     .name
-                        margin : 2px 0 8px 0
-                        height : 14px
-                        line-height : 14px
-                        font-size : 14px
-                        color : rgb(7, 17, 27)
+                        margin: 2px 0 8px 0
+                        height: 14px
+                        line-height: 14px
+                        font-size: 14px
+                        color: rgb(7, 17, 27)
                     .desc
-                        margin-bottom : 8px
-                        line-height : 10px
-                        font-size : 10px
-                        color :  rgb(147, 153, 159)
+                        margin-bottom: 8px
+                        line-height : 12px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
                     .extra
-                        line-height : 10px
-                        font-size : 10px
-                        color :  rgb(147, 153, 159)
+                        line-height: 10px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
                         .count
-                            margin-right : 12px
+                            margin-right: 12px
                     .price
-                        font-weight : 700
-                        line-height : 24px
+                        font-weight: 700
+                        line-height: 24px
                         .now
-                            margin-right : 8px
-                            font-size : 14px
-                            color : rgb(240, 20, 20)
+                            margin-right: 8px
+                            font-size: 14px
+                            color: rgb(240, 20, 20)
                         .old
-                            text-decoration : line-through
-                            font-size : 10px
-                            color : rgb(147, 153, 159)
+                            text-decoration: line-through
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
 </style>
