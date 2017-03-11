cf7fcd2ab5da66bf26cd30f00a251be7a9716218
Sellenite <714627528@qq.com>
2017-03-11 10:48:40 +0800

    shopcart complete

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,17 +3,16 @@
         <div class="content">
             <div class="content-left">
                 <div class="logo-wrapper">
-                    <div class="logo">
+                    <div class="logo" v-bind:class="{'highlight':totalCount!=0}">
                         <i class="icon-shopping_cart"></i>
                     </div>
+                    <div class="num" v-show="totalCount!=0">{{totalCount}}</div>
                 </div>
-                <div class="price">0元</div>
+                <div class="price" v-bind:class="{'highlight':totalPrice!=0}">￥{{totalPrice}}</div>
                 <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
             </div>
             <div class="content-right">
-                <div class="pay">
-                    ￥{{minPrice}}元起送
-                </div>
+                <div class="pay" v-bind:class="{'enough':totalPrice>=minPrice, 'not-enough':totalPrice<minPrice}">{{payDesc}}</div>
             </div>
         </div>
     </div>
@@ -22,13 +21,48 @@
 <script type="text/ecmascript-6">
     export default {
         props: {
+            selectFoods: {
+                type: Array,
+                default() {
+                    return [];
+                }
+            },
             deliveryPrice: {
                 type: Number,
                 default: 0
             },
             minPrice: {
                 type: Number,
-                default: 0
+                default: 20
+            }
+        },
+        computed: {
+            /* 计算总金额 */
+            totalPrice() {
+                let total = 0;
+                this.selectFoods.forEach((food) => {
+                    total += food.price * food.count;
+                });
+                return total;
+            },
+            /* 计算总数量 */
+            totalCount() {
+                let count = 0;
+                this.selectFoods.forEach((food) => {
+                    count += food.count;
+                });
+                return count;
+            },
+            /* 描述的变化 */
+            payDesc() {
+                if (this.totalPrice === 0) {
+                    return `￥${this.minPrice}元起送`;
+                } else if (this.totalPrice < this.minPrice) {
+                    let diff = this.minPrice - this.totalPrice;
+                    return `还差￥${diff}元起送`;
+                } else {
+                    return '去结算';
+                }
             }
         }
     };
@@ -36,67 +70,92 @@
 
 <style lang="stylus" rel="stylesheet/stylus">
     .shopcart
-        position : fixed
-        left : 0
-        bottom : 0
-        z-index : 50
-        width : 100%
-        height : 48px
+        position: fixed
+        left: 0
+        bottom: 0
+        z-index: 50
+        width: 100%
+        height: 48px
         .content
-            display : flex
-            font-size : 0
-            background : #141d27
+            display: flex
+            font-size: 0
+            background: #141d27
             .content-left
-                flex : 1
+                flex: 1
                 .logo-wrapper
-                    display : inline-block
-                    position : relative
-                    top : -10px
-                    margin : 0 12px
-                    padding : 6px
-                    width : 56px
-                    height : 56px
-                    box-sizing : border-box
-                    vertical-align : top
-                    border-radius : 50%
-                    background : #141d27
+                    display: inline-block
+                    position: relative
+                    top: -10px
+                    margin: 0 12px
+                    padding: 6px
+                    width: 56px
+                    height: 56px
+                    box-sizing: border-box
+                    vertical-align: top
+                    border-radius: 50%
+                    background: #141d27
                     .logo
-                        width : 100%
-                        height : 100%
-                        border-radius : 50%
-                        background : #2d343c
-                        text-align : center
+                        width: 100%
+                        height: 100%
+                        border-radius: 50%
+                        background: #2d343c
+                        text-align: center
+                        &.highlight
+                            background: rgb(0, 160, 220)
+                            .icon-shopping_cart
+                                color: #FFF
                         .icon-shopping_cart
-                            line-height : 44px
-                            font-size : 24px
-                            color : #80858A
+                            line-height: 44px
+                            font-size: 24px
+                            color: #80858A
+                    .num
+                        position: absolute
+                        top: 0
+                        right: 0
+                        width: 24px
+                        height: 16px
+                        line-height: 16px
+                        text-align: center
+                        border-radius: 16px
+                        font-size: 9px
+                        font-weight: 700
+                        color: #FFF
+                        background: rgb(240, 20, 20)
+                        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
                 .price
-                    display : inline-block
-                    vertical-align : top
-                    line-height : 24px
-                    margin-top : 12px
-                    box-sizing : border-box
-                    padding-right : 12px
-                    border-right : 1px solid rgba(255, 255, 255, 0.1)
-                    font-size : 16px
-                    font-weight : 700
-                    color : rgba(255, 255, 255, 0.4)
+                    display: inline-block
+                    vertical-align: top
+                    line-height: 24px
+                    margin-top: 12px
+                    box-sizing: border-box
+                    padding-right: 12px
+                    border-right: 1px solid rgba(255, 255, 255, 0.1)
+                    font-size: 16px
+                    font-weight: 700
+                    color: rgba(255, 255, 255, 0.4)
+                    &.highlight
+                        color: #FFF
                 .desc
-                    display : inline-block
-                    vertical-align : top
-                    line-height : 24px
-                    margin : 12px 0 0 12px
-                    color : rgba(255, 255, 255, 0.4)
-                    font-size : 10px
+                    display: inline-block
+                    vertical-align: top
+                    line-height: 24px
+                    margin: 12px 0 0 12px
+                    color: rgba(255, 255, 255, 0.4)
+                    font-size: 10px
             .content-right
-                flex : 0 0 105px
-                width : 105px
+                flex: 0 0 105px
+                width: 105px
                 .pay
-                    height : 48px
-                    line-height : 48px
-                    text-align : center
-                    font-size : 12px
-                    color : rgba(255, 255, 255, 0.4)
-                    font-weight : 700
-                    background : #2b333b
+                    height: 48px
+                    line-height: 48px
+                    text-align: center
+                    font-size: 12px
+                    color: rgba(255, 255, 255, 0.4)
+                    font-weight: 700
+                    background: #2b333b
+                    &.enough
+                        background : #00b43c
+                        color : #FFF
+                    &.not-enough
+                        background : #2b333b
 </style>
