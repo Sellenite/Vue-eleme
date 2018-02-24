339a36101cceb17781be7e3bec1a5957d2221730
Sellenite <714627528@qq.com>
2018-02-24 23:22:30 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div>
         <v-header v-bind:seller="seller"></v-header>
         <div class="tab border-1px">
             <div class="tab-item">
@@ -54,20 +54,19 @@ export default {
 
 <style lang="stylus" rel="stylesheet/stylus">
     @import 'common/stylus/mixin.styl'
-    #app
-        .tab
-            display: flex
-            width: 100%
-            height: 40px
-            line-height: 40px
-            border-1px(rgba(7, 17, 27, 0.1))
-            .tab-item
-                flex: 1
-                text-align: center
-                & > a
-                    display: block
-                    font-size: 14px
-                    color: rgb(77, 85, 93)
-                    &.active
-                        color: rgb(240, 20, 20)
+    .tab
+        display: flex
+        width: 100%
+        height: 40px
+        line-height: 40px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .tab-item
+            flex: 1
+            text-align: center
+            & > a
+                display: block
+                font-size: 14px
+                color: rgb(77, 85, 93)
+                &.active
+                    color: rgb(240, 20, 20)
 </style>
